e0aeec62c0d478b12b46e7d850855326d22b17e0
Rob <rob.mulla@gmail.com>
2016-07-30 18:48:38 -0400

    Updating from Robs PC

--- MITx.6.00.1x-Week7-AdoptionProject.py
+++ MITx.6.00.1x-Week7-AdoptionProject.py
@@ -168,6 +168,7 @@ class MedicatedAllergicAdopter(AllergicAdopter):
         score = medicine_multiplier * adoption_center.get_number_of_species(self.desired_species)
         return score
 
+
 class SluggishAdopter(Adopter):
     """
     A SluggishAdopter really dislikes travelleng. The further away the
@@ -208,11 +209,15 @@ class SluggishAdopter(Adopter):
 
         return adopter_score
 
+
 def get_ordered_adoption_center_list(adopter, list_of_adoption_centers):
     """
     The method returns a list of an organized adoption_center such that the scores for each AdoptionCenter to the Adopter will be ordered from highest score to lowest score.
     """
     # Your Code Here
+    centers_sorted = list_of_adoption_centers
+    centers_sorted.sort(key=adopter.get_score, reverse=True)
+    return centers_sorted
 
 
 def get_adopters_for_advertisement(adoption_center, list_of_adopters, n):
@@ -220,3 +225,77 @@ def get_adopters_for_advertisement(adoption_center, list_of_adopters, n):
     The function returns a list of the top n scoring Adopters from list_of_adopters (in numerical order of score)
     """
     # Your Code Here
+    sorted_adopters = list_of_adopters
+    # for x in list_of_adopters:
+    #     print "the adopter named "
+    #     print str(x.get_name)
+    #     print " has a score of "
+    #     print x.get_score(adoption_center)
+
+    sorted_adopters.sort(key=lambda adopting: (-adopting.get_score(adoption_center), adopting.get_name()))
+    sorted_adopters = sorted_adopters[:n]
+    return sorted_adopters
+
+""" BELOW IS TEST CODE """
+
+# adopter = MedicatedAllergicAdopter("One", "Cat", ['Dog', 'Horse'], {"Dog": .5, "Horse": 0.2})
+# adopter2 = Adopter("Two", "Cat")
+# adopter3 = FlexibleAdopter("Three", "Horse", ["Lizard", "Cat"])
+# adopter4 = FearfulAdopter("Four","Cat","Dog")
+# adopter5 = SluggishAdopter("Five","Cat", (1,2))
+# adopter6 = AllergicAdopter("Six", "Cat", "Dog")
+#
+# ac = AdoptionCenter("Place1", {"Mouse": 12, "Dog": 2}, (1,1))
+# ac2 = AdoptionCenter("Place2", {"Cat": 12, "Lizard": 2}, (3,5))
+# ac3 = AdoptionCenter("Place3", {"Horse": 25, "Dog": 9}, (-2,10))
+
+""" TEST 1 """
+adopter = MedicatedAllergicAdopter("One", "Cat", ['Dog', 'Horse'], {"Dog": .5, "Horse": rand.random()})
+adopter2 = Adopter("Two", "Cat")
+adopter3 = FlexibleAdopter("Three", "Horse", ["Lizard", "Cat"])
+adopter4 = FearfulAdopter("Four", "Cat", "Dog")
+adopter5 = SluggishAdopter("Five", "Cat", (1, 2))
+adopter6 = AllergicAdopter("Six", "Lizard", "Cat")
+
+ac = AdoptionCenter("Place1", {"Cat": 12, "Dog": 2}, (1, 1))
+ac2 = AdoptionCenter("Place2", {"Cat": 12, "Lizard": 2}, (3, 5))
+ac3 = AdoptionCenter("Place3", {"Cat": rand.randint(20, 50), "Dog": rand.randint(1, 10)}, (-2, 10))
+
+
+# how to test get_adopters_for_advertisement
+advertise = get_adopters_for_advertisement(ac, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10)
+advertise2 = get_adopters_for_advertisement(ac2, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10)
+advertise3 = get_adopters_for_advertisement(ac3, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10)
+
+print "Testing: ac, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10"
+for x in advertise:
+    print x.get_name()
+    print x.get_score(ac)
+
+print "Testing: ac2, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10"
+for x in advertise2:
+    print x.get_name()
+    print x.get_score(ac2)
+
+print "Testing: ac3, [adopter, adopter2, adopter3, adopter4, adopter5, adopter6], 10"
+for x in advertise3:
+    print x.get_name()
+    print x.get_score(ac3)
+
+# you can print the name and score of each item in the list returned
+
+adopter4 = FearfulAdopter("Four","Cat","Dog")
+adopter5 = SluggishAdopter("Five","Cat", (1,2))
+adopter6 = AllergicAdopter("Six", "Lizard", "Cat")
+
+ac = AdoptionCenter("Place1", {"Cat"
+                               "": 12, "Dog": 2}, (1,1))
+ac2 = AdoptionCenter("Place2", {"Cat": 12, "Lizard": 2}, (3,5))
+ac3 = AdoptionCenter("Place3", {"Cat": 40, "Dog": 4}, (-2,10))
+ac4 = AdoptionCenter("Place4", {"Cat": 33, "Horse": 5}, (-3,0))
+ac5 = AdoptionCenter("Place5", {"Cat": 45, "Lizard": 2}, (8,-2))
+ac6 = AdoptionCenter("Place6", {"Cat": 23, "Dog": 7, "Horse": 5}, (-10,10))
+
+# how to test get_ordered_adoption_center_list
+get_ordered_adoption_center_list(adopter4, [ac,ac2,ac3,ac4,ac5,ac6])
+# you can print the name and score of each item in the list returned
